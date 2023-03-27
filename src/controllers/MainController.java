28731e28d104096b2494fd2f70f51172ac515ad6
SulanAdam <sulan.adam19@gmail.com>
2023-03-27 16:38:32 +0200

    az első változat kész

---
+++ src/controllers/MainController.java
@@ -0,0 +1,21 @@
+package controllers;
+
+import javafx.collections.ObservableList;
+import models.Employee;
+import models.MainModel;
+import views.MainView;
+
+public class MainController {
+    MainView mainView;
+    MainModel mainModel;
+    public MainController() {
+        this.mainView=new MainView();
+        this.mainModel=new MainModel();
+        //ObservableList<Employee> empList=this.mainModel.getEmployee();
+        this.mainView.getTablePanel().setItems(this.mainModel.getEmployee());
+    }
+    public MainView getMainView() {
+        return mainView;
+    }
+    
+}
